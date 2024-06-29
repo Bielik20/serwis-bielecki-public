33ceaf2b2b58f7e1192f6dea6e353b0f5b210060
Damian Bielecki <bielik20@gmail.com>
2024-06-29 15:30:11 +0200

    9

--- apps/junkers/src/app/page.tsx
+++ apps/junkers/src/app/page.tsx
@@ -1,4 +1,8 @@
-import { Component } from '@serwis-bielecki-public/components/component';
+import { About } from '@serwis-bielecki-public/components/sections/about';
+import { Area } from '@serwis-bielecki-public/components/sections/area';
+import { Contact } from '@serwis-bielecki-public/components/sections/contact';
+import { HomeJunkers } from '@serwis-bielecki-public/components/sections/home-junkers';
+import { Services } from '@serwis-bielecki-public/components/sections/services';
 
 export default function Index() {
   /*
@@ -6,5 +10,13 @@ export default function Index() {
    *
    * Note: The corresponding styles are in the ./index.tailwind file.
    */
-  return <Component />;
+  return (
+    <>
+      <HomeJunkers />
+      <About />
+      <Services />
+      <Area />
+      <Contact />
+    </>
+  );
 }
